7d6c99fbd3e03c76264746a6f474dedd1a775037
Jason  Mulderrig <mulderrig.jason@gmail.com>
2024-03-25 17:41:13 -0400

    Copied in base files and code files. Started to modify code files.

--- README.md
+++
@@ -1,1 +0,0 @@
-# cuFJC-scission
---
+++ virtual-environment-install-master.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Make sure the VENV_PATH variable is correctly set based upon the computer system being used, as follows
+
+# For the workstation AnalyticalEngine
+VENV_PATH="/home/jason_mulderrig/research/projects/composite-uFJC-scission"
+
+# # For the laptop WSL2 running Ubuntu 20.04
+# VENV_PATH="/home/jasonmulderrig/research/projects/temp"
+
+# # For the Macbook Air
+# VENV_PATH="/Users/jasonmulderrig/research/projects/composite-uFJC-scission"
+
+# Set up Python virtual environment and associated Python packages
+
+if [ ! -d ${VENV_PATH} ]
+then
+  mkdir -p ${VENV_PATH}
+  python3 -m venv ${VENV_PATH}
+  cd ${VENV_PATH}
+else
+  cd ${VENV_PATH}
+  if [ ! -f pyvenv.cfg ]
+  then
+    python3 -m venv ${VENV_PATH}
+  else
+    rm -rf bin include lib share && rm lib64 && rm pyvenv.cfg
+    python3 -m venv ${VENV_PATH}
+  fi
+fi
+
+source bin/activate
+
+pip3 install wheel && pip3 install --upgrade setuptools && pip3 install --upgrade pip
+pip3 install numpy scipy mpmath sympy matplotlib pynverse
+
+deactivate
